09e2d39d395b14f7a9fbad7bf12f7265453ab69c
Nicola <mercuri512@gmail.com>
2024-12-04 18:38:04 +0100

    Initial commit

---
+++ src/main/java/com/example/progettoorganigrammaaziendale/ProgettoOrganigrammaAziendaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.progettoorganigrammaaziendale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoOrganigrammaAziendaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoOrganigrammaAziendaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/progettoorganigrammaaziendale/ProgettoOrganigrammaAziendaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progettoorganigrammaaziendale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoOrganigrammaAziendaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
